c40b8587a3b4dd77357205ad43831a8fd786ac40
jeanandjack <c_li70655@fanshaweonline.ca>
2018-03-02 00:13:23 -0500

    thirdcommit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 (() => {
     console.log('svg set loaded');
 
+function svg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
 
-
-
+function svg(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+}
 })();
